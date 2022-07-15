42faa58176062b1c9149de8b5ef14c2d578f68c9
CandelaRivero <92227113+CandelaRivero@users.noreply.github.com>
2022-07-15 11:33:41 -0300

    Add files via upload

---
+++ Desafio database/Api/main.js
@@ -0,0 +1,37 @@
+// const socket = io.connect();
+
+import ClienteSql from './sql.js.js'
+import { options } from './DB/mariaDB.js.js'
+
+
+const sql = new ClienteSql(options)
+
+
+function makeHTML(mensajes) {
+    const date = new Date()
+  return mensajes
+    .map((elem, index) => {
+      return `<div>
+            <strong style="color: blue;">${elem.author}</strong>:
+            <em style="color:brown;" >${elem.date}</em> </div>`;
+    })
+    .join(" ");
+}
+
+function render(data) {
+  const html = makeHTML(data);
+  document.getElementById("mensajes").innerHTML = html;
+}
+
+socket.on("mensajes", (mensajes) => {
+  render(mensajes);
+});
+
+function addMessage(e) {
+  const mensaje = {
+    author: document.getElementById("email").value,
+    date: document.getElementById("texto").value,
+  };
+  socket.emit("nuevoMensaje", mensaje);
+  return false;
+}
