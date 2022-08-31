8db5e2a03cbee631d641f7e64a7f2c6a74c8346e
CandelaRivero <92227113+CandelaRivero@users.noreply.github.com>
2022-08-31 10:14:56 -0400

    Add files via upload

---
+++ Desafio log-in/src/contenedores/login.js
@@ -0,0 +1,37 @@
+// login
+
+const form = document.getElementById('loginForm');
+
+form.addEventListener('submit',evt=>{
+    evt.preventDefault();
+    let data = new FormData(form);
+    let obj = {}
+    data.forEach((value,key)=>obj[key]=value)
+    fetch('/api/sessions/login',{
+        method:'POST',
+        body:JSON.stringify(obj),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }).then(result=>result.json()).then(json=>console.log(json));
+    res.form().seend(`Bienvenido ${user.first_name}`)
+})
+
+// register
+// const form2 = document.getElementById('registerForm');
+
+// form2.addEventListener('submit',evt=>{
+//     evt.preventDefault();
+//     let data = new FormData(form);
+//     let obj = {}
+//     data.forEach((value,key)=>obj[key]=value)
+//     fetch('/api/sessions/register',{
+//         method:'POST',
+//         body:JSON.stringify(obj),
+//         headers:{
+//             "Content-Type":"application/json"
+//         }
+//     }).then(result=>result.json()).then(json=>console.log(json));
+// })
+
+// logout
